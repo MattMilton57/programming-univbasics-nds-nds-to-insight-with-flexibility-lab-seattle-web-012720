bfb8fae7d2fc42a5f4cdcacb0601c22f54812078
Matt Milton <mattmilton57@gmail.com>
2020-01-19 01:06:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -32,10 +32,10 @@ end
 
 
 # Your code after this point
-p directors_database[0][:movies][0]
+
+movies_collection=[]
 def movies_with_director_key(name, movies_collection)
- movie_with_director_name(directors_database[0][:movies][0])
- p movie_with_director_name
+
  
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
